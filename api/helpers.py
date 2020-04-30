9299d0c41763c0dfd0e6a02fb964e7e02e6a0e6b
Jayashrri <balaji.jayashrri@gmail.com>
2020-04-30 12:45:16 +0530

    Add support for multiple API keys

--- api/helpers.py
+++ api/helpers.py
@@ -14,56 +14,62 @@ from .models import Video
 def queryAPI(page_token, published_after):
     api_service_name = "youtube"
     api_version = "v3"
-    developer_key = "AIzaSyDwajwJQgOODechipgWdDmt8Hkisjkx9Ng"
+    developer_keys = []
+    for developer_key in developer_keys:
+        try:
+            youtube_object = apiclient.discovery.build(
+                api_service_name, api_version, developerKey=developer_key
+            )
 
-    youtube_object = apiclient.discovery.build(
-        api_service_name, api_version, developerKey=developer_key
-    )
-
-    request = youtube_object.search().list(
-        pageToken=page_token,
-        part="snippet",
-        eventType="completed",
-        maxResults=50,
-        order="date",
-        q="official",
-        relevanceLanguage="en",
-        type="video",
-        publishedAfter=published_after,
-    )
-    response = request.execute()
-
-    return response
+            request = youtube_object.search().list(
+                pageToken=page_token,
+                part="snippet",
+                eventType="completed",
+                maxResults=50,
+                order="date",
+                q="official",
+                relevanceLanguage="en",
+                type="video",
+                publishedAfter=published_after,
+            )
+            response = request.execute()
+        except apiclient.errors.HttpError:
+            continue
+        else:
+            return response
+    else:
+        return None
 
 
 def getLatestVideos():
     published_after = (datetime.utcnow() - timedelta(hours=12)).isoformat("T") + "Z"
     response = queryAPI("", published_after)
-    result = response["items"]
-    page_token = response.get("nextPageToken", None)
-
-    while page_token and (len(response["items"]) != 0):
-        response = queryAPI(page_token, published_after)
-        result.extend(response["items"])
+    if response:
+        result = response["items"]
         page_token = response.get("nextPageToken", None)
 
-    new_videos = []
+        while page_token and (len(response["items"]) != 0):
+            response = queryAPI(page_token, published_after)
+            result.extend(response["items"])
+            page_token = response.get("nextPageToken", None)
 
-    for i, video in enumerate(result, 1):
-        details = Video(
-            id=i,
-            video_id=video["id"]["videoId"],
-            title=video["snippet"]["title"],
-            description=video["snippet"]["description"],
-            published_at=parse_datetime(video["snippet"]["publishedAt"]),
-            thumbnail=video["snippet"]["thumbnails"]["default"]["url"],
-        )
+        new_videos = []
 
-        new_videos.append(details)
+        for i, video in enumerate(result, 1):
+            details = Video(
+                id=i,
+                video_id=video["id"]["videoId"],
+                title=video["snippet"]["title"],
+                description=video["snippet"]["description"],
+                published_at=parse_datetime(video["snippet"]["publishedAt"]),
+                thumbnail=video["snippet"]["thumbnails"]["default"]["url"],
+            )
 
-    current_videos = Video.objects.order_by("-published_at")
-    if (not current_videos) or (
-        current_videos and current_videos[0].video_id != new_videos[0].video_id
-    ):
-        Video.objects.all().delete()
-        Video.objects.bulk_create(new_videos)
+            new_videos.append(details)
+
+        current_videos = Video.objects.order_by("-published_at")
+        if (not current_videos) or (
+            current_videos and current_videos[0].video_id != new_videos[0].video_id
+        ):
+            Video.objects.all().delete()
+            Video.objects.bulk_create(new_videos)
